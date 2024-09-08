8a9a39b30a81af959427325a7d0768fbdd987315
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-09-08 23:44:05 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
